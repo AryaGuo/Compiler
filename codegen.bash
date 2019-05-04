179881db26e094542071113b34e559fae89fd6b6
AryaGuo <gwx309@163.com>
2019-05-04 13:53:58 +0800

    fix codegen.bash

--- codegen.bash
+++ codegen.bash
@@ -7,5 +7,5 @@ set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Compiler.Main"
 cat > program.c   # save everything in stdin to program.txt
-cat < program.asm
 $CCHK
+cat < program.asm
